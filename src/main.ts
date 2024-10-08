a24adc2de907910b0ef619f9f7033cc5c417b546
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2024-10-09 04:50:36 +0600

    code fixed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import express from 'express';
+// Correct import for Express
+import * as express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
 
 const server = express(); // Initialize Express
